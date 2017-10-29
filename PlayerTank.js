fb7decb062d47d585d93331b6a93b6f60495d084
HjaltiDan <hjd1@hi.is>
2017-10-29 13:35:47 +0000

    PlayerTank: Adding slide and movement
    Cleaned up a lot of PlayerTank legacy code from Ship.js, and added
    better functionality for the slide and movement.

--- PlayerTank.js
+++ PlayerTank.js
@@ -47,7 +47,7 @@ PlayerTank.prototype.rememberResets = function () {
 
     //TODO: Decide how to handle rotation. Easiest hack right now
     //could be something like int 0,1,2,3 for up, down, left, right
-    this.reset_rotation = this.rotation;
+    this.reset_orientation = this.orientation;
 };
 
 PlayerTank.prototype.KEY_UP = 'W'.charCodeAt(0);
@@ -56,8 +56,10 @@ PlayerTank.prototype.KEY_LEFT   = 'A'.charCodeAt(0);
 PlayerTank.prototype.KEY_RIGHT  = 'D'.charCodeAt(0);
 PlayerTank.prototype.KEY_FIRE   = ' '.charCodeAt(0);
 
-// Initial, inheritable, default values
-PlayerTank.prototype.rotation = 0; //HD:Up
+PlayerTank.prototype.NORTH = 0;
+PlayerTank.prototype.SOUTH = 1;
+PlayerTank.prototype.WEST = 2;
+PlayerTank.prototype.EAST = 3;
 
 //TODO: Initialize xy coords based on correct location for tank in environment
 PlayerTank.prototype.cx = 200;
@@ -68,31 +70,32 @@ PlayerTank.prototype.cy = 200;
 PlayerTank.prototype.halfHeight = 50;
 PlayerTank.prototype.halfWidth = 50;
 
-//HD: The tank has no ongoing velocity ... unless it's on ice (or sand, or
-//whatever). When that happens, we adjust the slideFactor accordingly.
-PlayerTank.prototype.velX = 0;
-PlayerTank.prototype.velY = 0;
-PlayerTank.prototype.slideFactor = 1;
+//HD: This is set to a positive integer when the tank is on ice, and is used
+//to increment its movement. It is then decremented with each du
+PlayerTank.prototype.slideCounter = 0;
 
-PlayerTank.prototype.numSubSteps = 1;
+//HD: This is how far you move in a single step, either through a keypress
+//or by sliding on ice. (I picked the value simply because it's what part
+//used for the thrust value in Asteroids)
+PlayerTank.prototype.moveDistance = 0.2
+
+//HD: Starting off facing up.
+PlayerTank.prototype.orientation = NORTH;
+
+
+//HD: Commenting out for now - we likely don't need this
+//PlayerTank.prototype.numSubSteps = 1;
 
 // HACKED-IN AUDIO (no preloading)
 PlayerTank.prototype.warpSound = new Audio(
     "sounds/shipWarp.ogg");
 
-PlayerTank.prototype.warp = function () {
-
-    this._scaleDirn = -1;
-
-//HD NB:Commenting this out so we don't fail by trying to play a nonexistent
-//sound, but keeping it to show sound-playing functionality that we might want
-//to use later:
-//    this.warpSound.play();
-
-    // Unregister me from my old posistion
-    // ...so that I can't be collided with while warping
-    spatialManager.unregister(this);
-};
+//HD NB: We're not using this function right now, but I'm adding it just to
+//show how the code would look
+//Playertank.prototype.playSound = function()
+//{
+//  this.warpSound.play();
+//}
 
 
 PlayerTank.prototype.update = function (du) {
@@ -101,124 +104,98 @@ PlayerTank.prototype.update = function (du) {
   if (this._isDeadNow)
     return entityManager.KILL_ME_NOW;
 
-  //HD: Check if we're driving into anything. If not, move.
-  //NB: Do we need to perform the hitEntity() check for *every* substep?
-  var hitEntity = this.findHitEntity();
-  if (!hitEntity)
+  var sliding = false;
+  if(this.slideCounter > 0)
+    sliding = true;
+
+  if(sliding)
   {
-    // Perform movement substeps
-    var steps = this.numSubSteps;
-    var dStep = du / steps;
-    for (var i = 0; i < steps; ++i)
+    //HD NB: It's possible we may have to check first for a keypress,
+    //to see if the orientation has changed. Let's try this version for now,
+    //since it makes for simpler code.
+    switch(this.orientation)
     {
-        this.computeSubStep(dStep);
+      case(NORTH):
+        this.move(this.cx, this.cy - this.moveDistance);
+        break;
+      case(SOUTH):
+        this.move(this.cx, this.cy + this.moveDistance);
+        break;
+      case(WEST):
+        this.move(this.cx - this.moveDistance, this.cy);
+        break;
+      case(EAST):
+        this.move(this.cx + this.moveDistance, this.cy);
+        break;
     }
+    this.SlideCounter -= 1;
   }
 
-  //HD: Handle firing. Remember, we can fire even if we can't move.
+  //Check for keypress, but don't move if you've already slid.
+  if (keys[this.KEY_UP])
+  {
+    this.orientation = NORTH;
+    if(!sliding)
+      this.move(this.cx, this.cy - this.moveDistance);
+  }
+  if (keys[this.KEY_DOWN])
+  {
+    this.orientation = SOUTH;
+    if(!sliding)
+      this.move(this.cx, this.cy + this.moveDistance);
+
+  }
+  if (keys[this.KEY_LEFT])
+  {
+    this.orientation = WEST;
+    if(!sliding)
+        this.move(this.cx - this.moveDistance, this.cy);
+  }
+  if (keys[this.KEY_RIGHT])
+  {
+    this.orientation = EAST;
+    if(!sliding)
+      this.move(this.cx + this.moveDistance, this.cy);
+  }
+
+  //HD: Handle firing. (Remember that we can fire even if we can't move.)
   this.maybeFireBullet();
 
   spatialManager.register(this);
 
 };
 
-PlayerTank.prototype.computeSubStep = function (du) {
+PlayerTank.prototype.move = function(du, newX, newY)
+{
 
-  var thrustX, thrustY;
-
-  //HD: I got the 0.2 number from the old NOMINAL_THRUST in Ship.js.
-  //Going with that until we can fine-tune it.
-  if (keys[this.KEY_UP])
+  //HD: Check if we're driving into anything. If not, move.
+  var hitEntity = this.findHitEntity(moveX, moveY);
+  if (!hitEntity)
   {
-      thrustX = 0;
-      thrustY = -0.2;
+    this.cx = moveX;
+    this.cy = moveY;
+    //HD: Old substep code. Commenting it out in case we need it for
+    //reference later
+    //var steps = this.numSubSteps;
+    //var dStep = du / steps;
+    //for (var i = 0; i < steps; ++i)
+    //{
+    //    this.computeSubStep(dStep);
+    //}
   }
-  if (keys[this.KEY_DOWN])
-  {
-      thrustX = 0;
-      thrustY = 0.2;
-  }
-  if (keys[this.KEY_LEFT])
-  {
-    thrustX = -0.2;
-    thrustY = 0;
-  }
-  if (keys[this.KEY_RIGHT])
-  {
-    thrustX = 0.2;
-    thrustY = 0;
-  }
-
-  //HD: Adjusting velocity depending on the terrain we're driving over
-  //(2x for ice, 0.5x for sand, etc). Note that "1" is normal movement.
-  var accelX = thrustX * this.computeSlide();
-  var accelY = thrustY * this.computeSlide();
-
-  this.applyAccel(accelX, accelY, du);
-
-  this.wrapPosition(); //HD: We don't need this one, right?
-
-  this.updateRotation(du);
-
-};
+}
 
 
-PlayerTank.prototype.computeSlide = function () {
-    //TODO: Make sure slideFactor gets updated according to
-    //what terrain the tank is on. Will probably have to do so
-    //through entityManager calling a setSlideFactor, so I'll
-    //add a dummy function for that below this one.
-    return this.slideFactor;
-};
-
-PlayerTank.prototype.setSlide = function (multiplier) {
-    //TODO: Make sure this gets called correctly in entityManager
-    //depending on what terrain we're driving over.
-    this.slideFactor *= multiplier;
-};
-
-
-PlayerTank.prototype.applyAccel = function (accelX, accelY, du) {
-
-  //HD NB: Not going to pull this apart right now, but eventually
-  //we'll probably have to.
-
-    // u = original velocity
-    var oldVelX = this.velX;
-    var oldVelY = this.velY;
-
-    // v = u + at
-    this.velX += accelX * du;
-    this.velY += accelY * du;
-
-    // v_ave = (u + v) / 2
-    var aveVelX = (oldVelX + this.velX) / 2;
-    var aveVelY = (oldVelY + this.velY) / 2;
-
-    // Decide whether to use the average or not (average is best!)
-    var intervalVelX = g_useAveVel ? aveVelX : this.velX;
-    var intervalVelY = g_useAveVel ? aveVelY : this.velY;
-
-    // s = s + v_ave * t
-    var nextX = this.cx + intervalVelX * du;
-    var nextY = this.cy + intervalVelY * du;
-
-    // s = s + v_ave * t
-    this.cx += du * intervalVelX;
-    this.cy += du * intervalVelY;
-};
 
 PlayerTank.prototype.maybeFireBullet = function () {
 
-    if (keys[this.KEY_FIRE]) {
+  if (keys[this.KEY_FIRE]) {
 
-      //HD:Hacking this with simple, readable code for now:
-      //Rotation is 0,1,2,3 for up,down,left,right.
-      //We can do a fancy array switch() (or whatever) later
-      var turretX, turretY, velX, velY;
+    var turretX, turretY, velX, velY;
 
-      if(this.rotation === 0) //Turret pointed up
-      {
+    switch(this.orientation)
+    {
+      case(NORTH):
         //HD: We *could* simply use the old this.radius - it'd work
         //just fine for a perfect square - but we'll need height and width
         //anyway for collision detection.
@@ -226,27 +203,25 @@ PlayerTank.prototype.maybeFireBullet = function () {
         turretY = this.cY - this.halfHeight;
         velX = 0;
         velY = -2;
-      }
-      else if(this.rotation === 1) //Turret pointed down
-      {
+        break;
+      case(SOUTH):
         turretX = this.cx;
         turretY = this.cY + this.halfHeight;
         velX = 0;
         velY = 2;
-      }
-      else if(this.rotation === 2) //Turret pointed left
-      {
+        break;
+      case(WEST):
         turretX = this.cx - this.halfWidth;
         turretY = this.cY;
         velX = -2;
         velY = 0;
-      }
-      else if(this.rotation === 3) //Turret pointed right
-      {
+        break;
+      case(EAST):
         turretX = this.cx + this.halfWidth;
         turretY = this.cY;
         velX = 2;
         velY = 0;
+        break;
       }
 
       //HD NB: The fireBullet function variables may also need to include
@@ -264,7 +239,6 @@ PlayerTank.prototype.maybeFireBullet = function () {
 PlayerTank.prototype.takeBulletHit = function () {
   //TODO: Code accordingly. Do we call this only when we've been hit by an
   //enemy's bullet, or by any bullet?
-  //This used just call this.warp(), but obviously that no longer applies.
 
 };
 
@@ -275,26 +249,23 @@ PlayerTank.prototype.reset = function () {
     this.halt();
 };
 
-PlayerTank.prototype.halt = function () {
-    this.velX = 0;
-    this.velY = 0;
-};
-
+//HD: Commenting this out for now, probably won't need it
+/*
 PlayerTank.prototype.updateRotation = function (du) {
   if (keys[this.KEY_UP]) {
-      this.rotation = 0;
+      this.rotation = NORTH;
   }
   if (keys[this.KEY_DOWN]) {
-      this.rotation = 1;
+      this.rotation = SOUTH;
   }
   if (keys[this.KEY_LEFT]) {
-      this.rotation = 2;
+      this.rotation = EAST;
   }
   if (keys[this.KEY_RIGHT]) {
-      this.rotation = 3;
+      this.rotation = WEST;
   }
 };
-
+*/
 PlayerTank.prototype.render = function (ctx) {
     var origScale = this.sprite.scale;
     // pass my scale into the sprite, for drawing
