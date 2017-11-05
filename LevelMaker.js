88975ba186a795aff25618daec2cfbcbc013b969
Wolfcoder13 <wolfninja13@gmail.com>
2017-11-05 14:41:27 +0000

    Level maker almost ready

--- LevelMaker.js
+++ LevelMaker.js
@@ -1,62 +1,64 @@
 // ====
-// Brick
+// Levels jerry, levels!
 // ====
 
 "use strict";
 
 /* jshint browser: true, devel: true, globalstrict: true */
 
+var g_level_start_x = 0;
+var g_level_start_y = 0;
+var g_levelWidth = g_canvas.width;
+var g_levelHeight = g_canvas.height;
+var g_gridMultiplier = 26;
+var g_first_step = g_level_start_x + g_levelWidth/g_gridSize/2;
+var g_gridStep = g_levelWidth/g_gridSize;
 
 //level is 26 X 26 small bricks
 function createLevel(array) {
     // just for testing
-    for (i = 0; i < 26; i++){
-        for(j = 0; j < 26; j++){
+    for (var i = 0; i < g_gridSize; i++){
+        for(var j = 0; j < 13; j++){
             switch(array[i][j])
                 {
                     case(0):
                         entityManager.generateBrick({
-                            x      : 200,
-                            y      : 300,
-                            type   : consts.STRUCTURE_BRICK
-                            look   : consts.STRUCTURE_WHOLE
+                            cx      : g_first_step + g_gridStep*j,
+                            cy      : g_first_step + g_gridStep*i,
+                            type    : consts.STRUCTURE_BRICK,
+                            look    : consts.STRUCTURE_WHOLE
                         });
                     break;
                     case(1):
                         entityManager.generateBrick({
-                            x      : 200,
-                            y      : 300,
-                            type   : consts.STRUCTURE_STEEL
-                            look   : consts.STRUCTURE_WHOLE
+                            cx      : g_first_step + g_gridStep*j,
+                            cy      : g_first_step + g_gridStep*i,
+                            type    : consts.STRUCTURE_STEEL,
+                            look    : consts.STRUCTURE_WHOLE
                         });
                     break;
                     case(2):
                         entityManager.generateTerrain({
-                            x      : 200,
-                            y      : 300,
-                            type   : consts.TERRAIN_WATER
+                            cx      : g_first_step + g_gridStep*j,
+                            cy      : g_first_step + g_gridStep*i,
+                            type    : consts.TERRAIN_WATER
                         });
                     break;
                     case(3):
                         entityManager.generateTerrain({
-                            x      : 200,
-                            y      : 300,
-                            type   : consts.TERRAIN_TREES
+                            cx      : g_first_step + g_gridStep*j,
+                            cy      : g_first_step + g_gridStep*i,
+                            type    : consts.TERRAIN_TREES
                         });
                     break;
                     case(4):
                         entityManager.generateTerrain({
-                            x      : 200,
-                            y      : 300,
-                            type   : consts.TERRAIN_ICE
+                            cx      : g_first_step + g_gridStep*j,
+                            cy      : g_first_step + g_gridStep*i,
+                            type    : consts.TERRAIN_ICE
                         });
                     break;
                     case(5):
-                        entityManager.generateTerrain({
-                            x      : 200,
-                            y      : 300,
-                            type   : consts.TERRAIN_BLANK
-                        });
                     break;
                     
 
