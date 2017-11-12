6c9a0d8a306909d1ab54fa128d1c8f043ebef2cb
HjaltiDan <hjd1@hi.is>
2017-11-12 17:39:18 +0000

    Changed consts values
    Changing consts values so that we can differentiate between tanks and
    powerups (for points assignment in the entity manager)

--- consts.js
+++ consts.js
@@ -14,57 +14,57 @@ var consts = {
     DIRECTION_DOWN  : 2,
     DIRECTION_LEFT  : 3,
 
-    TANK_PLAYER1        : 1,
-    TANK_PLAYER2        : 2,
-    TANK_ENEMY_BASIC    : 3,
-    TANK_ENEMY_FAST     : 4,
-    TANK_ENEMY_POWER    : 5,
-    TANK_ENEMY_ARMOR    : 6,
+    TANK_PLAYER1        : 4,
+    TANK_PLAYER2        : 5,
+    TANK_ENEMY_BASIC    : 6,
+    TANK_ENEMY_FAST     : 7,
+    TANK_ENEMY_POWER    : 8,
+    TANK_ENEMY_ARMOR    : 9,
 
     //HD: These are permanent looks for tanks that they keep until they die.
     //They are used to deliver specific tank sprites (i.e. not sprites of
     //powerups).The "DROPSPOWERUP" is for certain unique enemy tank sprites.
-    TANK_POWER_NONE         : 0,
-    TANK_POWER_1STAR        : 1,
-    TANK_POWER_2STARS       : 2,
-    TANK_POWER_3STARS       : 3,
-    TANK_POWER_DROPSPOWERUP : 4,
+    TANK_POWER_NONE         : 10,
+    TANK_POWER_1STAR        : 11,
+    TANK_POWER_2STARS       : 12,
+    TANK_POWER_3STARS       : 13,
+    TANK_POWER_DROPSPOWERUP : 14,
 
-    STRUCTURE_BRICK : 0,
-    STRUCTURE_STEEL : 1,
+    STRUCTURE_BRICK : 15,
+    STRUCTURE_STEEL : 16,
     //HD: The flag can't be driven over, so I'm filing it with other structures
-    STRUCTURE_FLAG  : 2,
+    STRUCTURE_FLAG  : 17,
 
     //HD: These are basically "direction" for structures.
-    STRUCTURE_WHOLE         : 0,
-    STRUCTURE_LEFT_GONE     : 1,
-    STRUCTURE_TOP_GONE      : 2,
-    STRUCTURE_BOTTOM_GONE   : 3,
-    STRUCTURE_RIGHT_GONE    : 4,
-    STRUCTURE_ALL_GONE      : 5,
+    STRUCTURE_WHOLE         : 18,
+    STRUCTURE_LEFT_GONE     : 19,
+    STRUCTURE_TOP_GONE      : 20,
+    STRUCTURE_BOTTOM_GONE   : 21,
+    STRUCTURE_RIGHT_GONE    : 22,
+    STRUCTURE_ALL_GONE      : 23,
     //HD: Note that we have two uses for ALL_GONE: One for a structure that
     //no longer has collision and will be drawn as a blank; and another for the
     //flag when it has been destroyed.
 
     //HD NOTE: Water is the only terrain that's constantly animated
     //EAH: changing these values to correspond to those used in LevelMaker
-    TERRAIN_WATER   : 2,
-    TERRAIN_TREES   : 3,
-    TERRAIN_ICE     : 4,
-    TERRAIN_BLANK   : 5,
+    TERRAIN_WATER   : 24,
+    TERRAIN_TREES   : 25,
+    TERRAIN_ICE     : 26,
+    TERRAIN_BLANK   : 27,
 
-    POWERUP_HELMET  : 0,
-    POWERUP_TIMER   : 1,
-    POWERUP_SHOVEL  : 2,
-    POWERUP_STAR    : 3,
-    POWERUP_GRENADE : 4,
-    POWERUP_TANK    : 5,
+    POWERUP_HELMET  : 28,
+    POWERUP_TIMER   : 29,
+    POWERUP_SHOVEL  : 30,
+    POWERUP_STAR    : 31,
+    POWERUP_GRENADE : 32,
+    POWERUP_TANK    : 33,
 
-    EFFECT_SPAWNFLASH       : 0,
-    EFFECT_SMALLEXPLOSION   : 1,
-    EFFECT_LARGEEXPLOSION   : 2,
-    EFFECT_INVULNERABLE     : 3,
-    EFFECT_POINTS          : 4
+    EFFECT_SPAWNFLASH       : 34,
+    EFFECT_SMALLEXPLOSION   : 35,
+    EFFECT_LARGEEXPLOSION   : 36,
+    EFFECT_INVULNERABLE     : 37,
+    EFFECT_POINTS          : 38
 
 
 
